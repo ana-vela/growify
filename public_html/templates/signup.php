4f955802264cd2d3b461b14cb0947df7e9a42e81
Ana Vela <avela7@cnm.edu>
2016-11-30 11:31:14 -0700

    added static template for sign up page https://app.asana.com/0/201954860780444/221812656877037

--- public_html/templates/signup.php
+++ public_html/templates/signup.php
@@ -1,8 +1,48 @@
-<section class="signup">
-	<div class="container">
-		<div class="jumbotron text-center">
-			<h1>Sign Up Page</h1>
-			<p class="lead">Sign Up to Growify your garden! </p>
+
+<div class="container">
+	<div class="row centered-form">
+		<div class="col-xs-12 col-sm-8 col-md-4 col-sm-offset-2 col-md-offset-4">
+			<div class="panel panel-default">
+				<div class="panel-heading">
+					<h3 class="panel-title text-center">Please sign up for Growify!</h3>
+				</div>
+				<div class="panel-body">
+					<form role="form">
+						<div class="row">
+							<div class="col-xs-6 col-sm-6 col-md-6">
+								<div class="form-group">
+									<input type="text" name="user_name" id="user_name" class="form-control input-sm" placeholder="User Name">
+								</div>
+							</div>
+							<div class="col-xs-6 col-sm-6 col-md-6">
+								<div class="form-group">
+									<input type="text" name="zip_code" id="zip_code" class="form-control input-sm" placeholder="Zip Code">
+								</div>
+							</div>
+						</div>
+
+						<div class="form-group">
+							<input type="email" name="email" id="email" class="form-control input-sm" placeholder="Email Address">
+						</div>
+
+						<div class="row">
+							<div class="col-xs-6 col-sm-6 col-md-6">
+								<div class="form-group">
+									<input type="password" name="password" id="password" class="form-control input-sm" placeholder="Password">
+								</div>
+							</div>
+							<div class="col-xs-6 col-sm-6 col-md-6">
+								<div class="form-group">
+									<input type="password" name="password_confirmation" id="password_confirmation" class="form-control input-sm" placeholder="Confirm Password">
+								</div>
+							</div>
+						</div>
+
+						<input type="submit" value="Register" class="btn btn-info btn-block">
+
+					</form>
+				</div>
+			</div>
 		</div>
 	</div>
-</section>
+</div>
