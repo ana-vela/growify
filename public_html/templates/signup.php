dd906862c422fcf2ba99e89a7d9aa06aeb0a6ead
Ana Vela <avela7@cnm.edu>
2016-11-30 13:34:14 -0700

    adjusted confirm password field in static template for sign up page https://app.asana.com/0/201954860780444/221812656877037

--- public_html/templates/signup.php
+++ public_html/templates/signup.php
@@ -26,17 +26,20 @@
 						</div>
 
 						<div class="row">
-							<div class="col-xs-6 col-sm-6 col-md-6">
+							<div class="col-xs-12 col-sm-12 col-md-12">
 								<div class="form-group">
 									<input type="password" name="password" id="password" class="form-control input-sm" placeholder="Password">
 								</div>
 							</div>
-							<div class="col-xs-6 col-sm-6 col-md-6">
-								<div class="form-group">
-									<input type="password" name="password_confirmation" id="password_confirmation" class="form-control input-sm" placeholder="Confirm Password">
+						</div>
+							<div class="row">
+								<div class="col-xs-12 col-sm-12 col-md-12">
+									<div class="form-group">
+										<input type="password" name="password_confirmation" id="password_confirmation" class="form-control input-sm" placeholder="Confirm Password">
+									</div>
 								</div>
 							</div>
-						</div>
+
 
 						<input type="submit" value="Register" class="btn btn-info btn-block">
 
