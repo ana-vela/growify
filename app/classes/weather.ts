241aa21ceb0e584dfbe22bea4e8a027db14f77b2
Rebecca Dicharry <rdicharry@gmail.com>
2016-12-02 15:55:14 -0700

    changed syntax on Weather.getIconClass()

--- app/classes/weather.ts
+++ app/classes/weather.ts
@@ -2,12 +2,12 @@ export class Weather {
 	constructor(public currentTemperature: number, public temperatureMin: number, public temperatureMax: number, public windSpeed: number,public humidity: number, public precipitationProbability: number, public timestamp: number, public summary: string, public icon: string){}
 
 	getIconClass() {
-		let classes = {
+		return {
 			snow: this.icon == "snow",
 			rain: this.icon == "rain"
 			/* TODO add remaining classes */
-		}
-		return classes;
+		};
+
 	}
 
 }
