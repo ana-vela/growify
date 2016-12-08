3a98add9b8d4a2cefefc2bff7b6a2cca39a35a26
Greg Bloom <gbloom3@cnm.edu>
2016-12-08 09:37:29 -0700

    added update, create and delete methods and formatted code for readability

--- app/services/profile-service.ts
+++ app/services/profile-service.ts
@@ -14,18 +14,37 @@ export class ProfileService extends BaseService {
 
 	getProfileByUsername(profileUsername: string) : Observable<Profile>{
 
-	return(this.http.get(this.profileUrl+"?profileUserInput="+profileUsername).map(this.extractData).catch(this.handleError));
+	return(this.http.get(this.profileUrl+"?profileUserInput="+profileUsername)
+		.map(this.extractData)
+		.catch(this.handleError));
 
 }
 
 	getProfileByEmail(profileEmail: string) : Observable<Profile>{
 
-		return(this.http.get(this.profileUrl+"?profileUserInput="+profileEmail).map(this.extractData).catch(this.handleError));
+		return(this.http.get(this.profileUrl+"?profileUserInput="+profileEmail)
+			.map(this.extractData)
+			.catch(this.handleError));
 
 	}
-	putProfile(profile: Profile) : Observable<Profile>{
+	createProfile(profile: Profile) : Observable<Profile>{
 
-		return;
+		return(this.http.post(this.profileUrl, profile)
+			.map(this.extractMessage)
+			.catch(this.handleError));
 
+	}
+	updateProfile(profile: Profile) : Observable<Profile>{
+
+		return(this.http.put(this.profileUrl + profile.profileId, profile)
+			.map(this.extractMessage)
+			.catch(this.handleError));
+
+	}
+	deleteProfile(profile: Profile) : Observable<Profile>{
+
+		return(this.http.delete(this.profileUrl + profile.profileId)
+			.map(this.extractMessage)
+			.catch(this.handleError));
 	}
 }
