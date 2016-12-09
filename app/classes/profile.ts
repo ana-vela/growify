215f59b7d97f026fd2a79a6ba62ecc93a5136305
Greg Bloom <gbloom3@cnm.edu>
2016-12-09 09:40:17 -0700

    changed zipcode variable to match other references

--- app/classes/profile.ts
+++ app/classes/profile.ts
@@ -1,3 +1,3 @@
 export class Profile {
-	constructor(public profileId: number, public profileUsername: string, public profileZipcode: string, public profileEmail: string, public profilePassword: string){}
+	constructor(public profileId: number, public profileUsername: string, public profileZipCode: string, public profileEmail: string, public profilePassword: string){}
 }
