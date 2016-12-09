f36d5c02c257b45baeddfe0f93af7b4a6a944054
Greg Bloom <gbloom3@cnm.edu>
2016-12-09 08:55:01 -0700

    added password input to get methods for login flow

--- app/services/profile-service.ts
+++ app/services/profile-service.ts
@@ -3,6 +3,7 @@ import {Http} from "@angular/http";
 import {Observable} from "rxjs/Observable";
 import {BaseService} from "./base-service";
 import {Profile} from "../classes/profile";
+import {Status} from "../classes/status";
 
 @Injectable()
 export class ProfileService extends BaseService {
@@ -12,29 +13,29 @@ export class ProfileService extends BaseService {
 
 	private profileUrl = "api/profile/";
 
-	getProfileByUsername(profileUsername: string) : Observable<Profile>{
+	getProfileByUsername(profileUsername: string, profilePassword: string) : Observable<Profile>{
 
-	return(this.http.get(this.profileUrl+"?profileUserInput="+profileUsername)
+	return(this.http.get(this.profileUrl+"?profileUserInput="+profileUsername+",?profilePasswordInput="+profilePassword)
 		.map(this.extractData)
 		.catch(this.handleError));
 
 }
 
-	getProfileByEmail(profileEmail: string) : Observable<Profile>{
+	getProfileByEmail(profileEmail: string, profilePassword: string) : Observable<Profile>{
 
-		return(this.http.get(this.profileUrl+"?profileUserInput="+profileEmail)
+		return(this.http.get(this.profileUrl+"?profileUserInput="+profileEmail+",?profilePasswordInput="+profilePassword)
 			.map(this.extractData)
 			.catch(this.handleError));
 
 	}
-	createProfile(profile: Profile) : Observable<Profile>{
+	postProfile(profile: Profile) : Observable<Status> {
 
 		return(this.http.post(this.profileUrl, profile)
 			.map(this.extractMessage)
 			.catch(this.handleError));
 
 	}
-	updateProfile(profile: Profile) : Observable<Profile>{
+	putProfile(profile: Profile) : Observable<Profile>{
 
 		return(this.http.put(this.profileUrl + profile.profileId, profile)
 			.map(this.extractMessage)
