a3fe926a709fadb22d49ead972e9e932ced6147c
Greg Bloom <gbloom3@cnm.edu>
2016-12-10 12:11:18 -0700

    added logout api
    https://app.asana.com/0/201954860780444/228572941629503

---
+++ public_html/api/logout/index.php
@@ -0,0 +1,40 @@
+<?php
+require_once dirname(__DIR__, 2) . "/classes/autoload.php";
+require_once dirname(__DIR__, 2) . "/lib/xsrf.php";
+require_once("/etc/apache2/capstone-mysql/encrypted-config.php");
+use Edu\Cnm\BrewCrew;
+/**
+ * api for signing out
+ **/
+//prepare default error message
+$reply = new stdClass();
+$reply->status = 200;
+$reply->data = null;
+try {
+	//grab the mySQL connection
+	$pdo = connectToEncryptedMySQL("/etc/apache2/capstone-mysql/growify.ini");
+	//determine which HTTP method was used
+	$method = array_key_exists("HTTP_X_HTTP_METHOD", $_SERVER) ? $_SERVER["HTTP_X_HTTP_METHOD"] : $_SERVER["REQUEST_METHOD"];
+	if($method === "GET"){
+		if(session_status() !== PHP_SESSION_ACTIVE) {
+			session_start();
+		}
+		$_SESSION = [];
+		$reply->message = "You have signed out. Thank you for visiting Growify.";
+	}
+	else {
+		throw (new InvalidArgumentException("Invalid HTTP method request"));
+	}
+} catch(Exception $exception) {
+	$reply->status = $exception->getCode();
+	$reply->message = $exception->getMessage();
+} catch(TypeError $typeError) {
+	$reply->status = $typeError->getCode();
+	$reply->message = $typeError->getMessage();
+}
+header("Content-type: application/json");
+if($reply->data === null) {
+	unset($reply->data);
+}
+// encode and return reply to front end caller
+echo json_encode($reply);
