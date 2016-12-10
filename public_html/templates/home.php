cfe1e2f2912a74d9fd981e8714d178e8376cd1c4
Ana Vela <avela7@cnm.edu>
2016-12-09 21:46:37 -0700

    changed nav bar text to white https://app.asana.com/0/201954860780444/207117838482669

--- public_html/templates/home.php
+++ public_html/templates/home.php
@@ -1,24 +1,23 @@
 <section class="welcome">
 	<div class="home">
-			<div class="container-fluid text-center">
-				<img src="../app/img/vegetable-basket.jpg">
-<div class="splash-text">
-			<h1>Welcome to Growify!</h1>
-			<p class="lead">This is an app for supplying growing information for home gardeners!</p>
-			<!--<a routerLink="/weather">Weather</a>-->
-		</div>
-		<div class="row center-block">
-			<div class="col-md-2 col-md-offset-9">
-			<div class="row">
-			<button type="button" class="btn btn-primary center-block" data-toggle="modal" data-target="#login-modal">Login</button>
-			</div>
-		<div class="row">
-			<button type="button" class="btn btn-primary center-block" data-toggle="modal" data-target="#signup-modal">Sign Up</button>
-		</div>
-		</div>
-		</div>
-		</div>
+		<div class="container-fluid text-center">
 
+			<div>
+				<h1>Welcome to Growify!</h1>
+				<p class="lead">A Farmers' Almanac for Home Gardeners!</p>
+				<!--<a routerLink="/weather">Weather</a>-->
+			</div>
+			<div class="row center-block">
+				<div class="col-md-2 col-md-offset-9">
+					<div class="row">
+						<button type="button" class="btn btn-primary center-block" data-toggle="modal" data-target="#login-modal">Login</button>
+					</div>
+					<div class="row">
+						<button type="button" class="btn btn-primary center-block" data-toggle="modal" data-target="#signup-modal">Sign Up</button>
+					</div>
+				</div>
+			</div>
+		</div>
 	</div>
 	<login-component></login-component>
 	<signup-component></signup-component>
