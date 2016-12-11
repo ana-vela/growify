b4c0a69c3d617be9ff3c470f9c13a2e2e72554a5
Daniel Eaton <danieleaton747@gmail.com>
2016-12-11 07:57:20 -0700

    Added changes to the Garden Component, Service, and Template  to make it function

---
+++ app/classes/plantGarden.ts
@@ -0,0 +1,10 @@
+import {Garden} from "../classes/garden";
+import {Plant} from "../classes/plant";
+
+/**
+ * Created by Daniel Eaton on 12/10/2016.
+ */
+export class PlantGarden{
+	constructor(public garden:Garden, public plant:Plant){
+	}
+}
--- public_html/templates/garden.php
+++ public_html/templates/garden.php
@@ -1,4 +1,4 @@
-<!-- will need to populate with user's name -->
+<!-- will need to populate with user's name-->
 <h1>Millie's Garden</h1>
 <section class="weather-display">
 	<div class="container">
@@ -7,70 +7,41 @@
 				<weather-component></weather-component>
 			</div>
 		</div>
-		<div class="col-md-10 col-offset-1">
-		<h1>Test:</h1>
-			<h1 *ngFor="let item of garden">{{item.gardenDatePlanted}}</h1>
-		</div>
-	</div>
 </section>
-
-<section>
-	<div class="container">
-		<!--<div class="col-md-12">-->
-			<table class="table">
-				<tr>
-					<th>Plant Name</th>
-					<th>Date Planted</th>
-					<th></th>
-					<th></th>
-					<th></th>
-				</tr>
-				<tr>
-					<td>carrots</td>
-					<td>04/28/2016</td>
-					<td><i class="fa fa-leaf success-leaf fa-2x" aria-hidden="true"></i></td>
-					<td>
-						<div class="progress">
-							<div class="progress-bar progress-bar-success progress-bar-striped" role="progressbar" aria-valuenow="40" aria-valuemin="0" aria-valuemax="100" style="width: 40%">
-								<span class="sr-only">40% Complete (success)</span>
-							</div>
-						</div>
-					</td>
-					<td><i class="fa fa-trash" aria-hidden="true"></i></td>
-				</tr>
-				<tr>
-					<td>tomatoes</td>
-					<td>03/20/2016</td>
-					<td><i class="fa fa-leaf success-leaf fa-2x" aria-hidden="true"></i></td>
-					<td>
-						<div class="progress">
-							<div class="progress-bar progress-bar-success progress-bar-striped" role="progressbar" aria-valuenow="50" aria-valuemin="0" aria-valuemax="100" style="width: 40%"> 50%
-								<span class="sr-only">50% Complete (success)</span>
-							</div>
-						</div>
-					</td>
-					<td><i class="fa fa-trash" aria-hidden="true"></i></td>
-				</tr>
-				<tr>
-					<td>sunflowers</td>
-					<td>03/28/2016</td>
-					<td><i class="fa fa-snowflake-o fa-2x" aria-hidden="true"></i></td>
-					<td>
-						<div class="progress">
-							<div class="progress-bar progress-bar-success progress-bar-striped" role="progressbar" aria-valuenow="40" aria-valuemin="0" aria-valuemax="100" style="width: 40%">
-								<span class="sr-only">40% Complete (success)</span>
-							</div>
-						</div>
-					</td>
-					<td><i class="fa fa-trash" aria-hidden="true"></i></td>
-				</tr>
-			</table>
-			<div><!--(click)="toggleSelected(plant)" -->
+<div class="container">
+	<table class="table">
+		<tr>
+			<th>Plant Name</th>
+			<th>Date Planted</th>
+			<th></th>
+			<th></th>
+			<th></th>
+		</tr>
+		<tr *ngFor="let item of plantGarden">
+			<td>Plant Name of:{{item.plant.plantName}}</td>
+			<td>Date Planted of: {{item.garden.gardenDatePlanted}}</td>
+		</tr>
+</table>
+			<td [ngSwitch]="let item.plant.plantType of plantGarden">
+				<i [ngSwitchCase]="Vegetable" class="fa fa-car success-leaf fa-2x" aria-hidden="true"></i>
+				<i [ngSwitchCase]="Fruit" class="fa fa-car success-leaf fa-2x" aria-hidden="true"></i>
+				<i [ngSwitchDefault] class="fa fa-leaf success-leaf fa-2x" aria-hidden="true"></i>
+			</td>
+			<td>
+				<div class="progress">
+					<div class="progress-bar progress-bar-success progress-bar-striped" role="progressbar" aria-valuenow="40" aria-valuemin="0" aria-valuemax="100" style="width: 40%">
+						<span class="sr-only">40% Complete (success)</span>
+					</div>
+				</div>
+			</td>
+			<td><i class="fa fa-trash" aria-hidden="true"></i></td>
+		</tr>
+	</table>
+</div>
+			<div>(click)="toggleSelected(plant)"
 				<button class="btn btn-default" type="button">
 					<i class="fa fa-plus" aria-hidden="true"></i>
 				</button>
 				   Add a Plant
 			</div>
-		</div>
-	<!--</div>-->
 </section>
