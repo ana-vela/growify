f4c0d42431b9053cddf404ed5d5030da881514a7
Greg Bloom <gbloom3@cnm.edu>
2016-12-11 12:32:54 -0700

    removed providers from  garden component @rdicharry :heart_eyes_cat:! @Dannyeaton -Takes the blame

--- app/components/garden-component.ts
+++ app/components/garden-component.ts
@@ -11,29 +11,30 @@ import{PlantService} from "../services/plant-service";
 import {Plant} from "../classes/plant"
 import {PlantGarden} from "../classes/plantGarden";
 @Component({
-	templateUrl: "./templates/garden.php",
-	providers:[PlantGarden, Plant, Garden]
+	templateUrl: "./templates/garden.php"
 })
 
-export class GardenComponent implements OnInit{
-garden: Garden[]=[];
-plantGarden: PlantGarden[]=[];
-plant: Plant[] = [];
-	constructor(private gardenService: GardenService, private plantService: PlantService){
+export class GardenComponent implements OnInit {
+	garden: Garden[] = [];
+	plantGarden: PlantGarden[] = [];
+	plant: Plant[] = [];
+
+	constructor(private gardenService: GardenService, private plantService: PlantService) {
 	}
-	ngOnInit():void{
+
+	ngOnInit(): void {
 		alert("begin");
 		this.gardenService.getGardenByGardenProfileId(34).subscribe(
-			(data)=>{
-				for(let count = 0; count < data.length; count++){
-					this.garden[count] = new Garden(data[count].gardenProfileId,data[count].gardenDatePlanted, data[count].gardenPlantId);
+			(data) => {
+				for(let count = 0; count < data.length; count++) {
+					this.garden[count] = new Garden(data[count].gardenProfileId, data[count].gardenDatePlanted, data[count].gardenPlantId);
 				}
-				for(let c=0; c< data.length; c++){
+				for(let c = 0; c < data.length; c++) {
 					this.plantService.getPlantByPlantId(this.garden[c].gardenPlantId).subscribe(
-						(data)=>{
+						(data) => {
 							this.plant[c] = new Plant(data.plantId, data.plantName, data.plantLatinName, data.plantVariety, data.plantType, data.plantDescription, data.plantSpread, data.plantHeight, data.plantDaysToHarvest, data.plantMinTemp, data.plantMaxTemp, data.plantSoilMoisture, false);
 							//plant Garden Initialization
-							this.plantGarden[c]= new PlantGarden(this.garden[c],this.plant[c]);
+							this.plantGarden[c] = new PlantGarden(this.garden[c], this.plant[c]);
 							alert(this.plantGarden[c].plant.plantType);
 						}
 					)
