857988754cde31f0f4e8f66d730bdd182c9732f8
Greg Bloom <gbloom3@cnm.edu>
2016-12-11 13:25:36 -0700

    added if block to check session to display certain components

--- public_html/templates/home.php
+++ public_html/templates/home.php
@@ -19,8 +19,11 @@
 			</div>
 		</div>
 	</div>
+	<?php if(empty($_SESSION["profile"]) === true) { ?>
 	<login-component></login-component>
 	<signup-component></signup-component>
+	<?php } else { ?>
 	<logout-component></logout-component>
+	<?php } ?>
 </section>
 
