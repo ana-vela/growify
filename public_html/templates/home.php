c1027d0c2dd3ac6856957d4d7e2aae30e8d02673
Ana Vela <avela7@cnm.edu>
2016-12-12 09:22:06 -0700

    corrected link colors for nav bar https://app.asana.com/0/201954860780444/228572237732863

--- public_html/templates/home.php
+++ public_html/templates/home.php
@@ -7,9 +7,9 @@
 				<p class="lead">A Farmers' Almanac for Home Gardeners!</p>
 				<!--<a routerLink="/weather">Weather</a>-->
 			</div>
-			<div class="row center-block">
+			<div class="row center-block btn-group">
 				<div class="col-md-2 col-md-offset-9">
-					<div class="row btn-group">
+					<div class="row">
 						<button type="button" class="btn btn-success center-block btn-default" data-toggle="modal" data-target="#login-modal">Login</button>
 					</div>
 					<div class="row">
